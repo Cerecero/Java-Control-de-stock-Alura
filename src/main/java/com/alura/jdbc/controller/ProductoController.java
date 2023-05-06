205b4327ffd86c5c180333c1610a2f83f00c6fe7
Cerecero <ale.rey.cer@outlook.com>
2023-05-05 19:47:48 -0600

    changed statement to PreparedStatement on the listar method

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -38,9 +38,10 @@ public class ProductoController {
 
 	public List<Map<String, String>> listar() throws SQLException {
 		Connection con = new ConnectionFactory().recuperaConexion();
-		Statement statement = con.createStatement();
+		PreparedStatement statement = con.prepareStatement(
+				"SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM producto");
 
-		boolean result = statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM producto");
+		boolean result = statement.execute();
 
 		ResultSet resultSet = statement.getResultSet();
 		List<Map<String, String>> resultado = new ArrayList<>();

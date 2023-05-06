7c53f89807ac8adfc707f63393847e2522fbb437
Cerecero <ale.rey.cer@outlook.com>
2023-05-05 19:46:23 -0600

    changed statement to PreparedStatement

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -62,14 +62,21 @@ public class ProductoController {
 	}
 
     public void guardar(Map<String, String> producto) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection con = factory.recuperaConexion();
 
-		Statement statement = con.createStatement();
+		PreparedStatement statement = con.prepareStatement(
+				"INSERT INTO producto(nombre, descripcion, cantidad)"
+						+ "(nombre, descripcion, cantidad)"
+						+ "VALUES (?,?,?)",
+				Statement.RETURN_GENERATED_KEYS);
 
-		statement.execute("INSERT INTO producto(nombre, descripcion, cantidad) "
-				+ "VALUES('" + producto.get("NOMBRE") + "','"
-				+producto.get("DESCRIPCION")+"',"
-				+producto.get("CANTIDAD")+ ")", Statement.RETURN_GENERATED_KEYS);
+		statement.setString(1, producto.get("NOMBRE"));
+		statement.setString(2, producto.get("DESCRIPCION"));
+		statement.setInt(3, Integer.valueOf(producto.get("CANTIDAD")));
+
+		statement.execute();
+
 		ResultSet resultSet = statement.getGeneratedKeys();
 
 		while (resultSet.next()){

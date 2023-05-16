7013f9f73325db55064387e9546db52f57c7d715
Cerecero <ale.rey.cer@outlook.com>
2023-05-16 16:45:03 -0600

    moved some logic from ProductoCOntroller to PersistenciaProducto

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.alura.jdbc.controller;
 
 import com.alura.jdbc.factory.ConnectionFactory;
 import com.alura.jdbc.modelo.Producto;
+import com.alura.jdbc.persistencia.PersistenciaProducto;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -69,47 +70,11 @@ public class ProductoController {
 	}
 
     public void guardar(Producto producto) throws SQLException {
+		PersistenciaProducto persistenciaProducto = new PersistenciaProducto(new ConnectionFactory().recuperaConexion());
 
-
-
-		ConnectionFactory factory = new ConnectionFactory();
-		Connection con = factory.recuperaConexion();
-
-
-		try(con) {
-			con.setAutoCommit(false);
-
-			try (PreparedStatement statement = con.prepareStatement(
-					"INSERT INTO producto" + "(nombre, descripcion, cantidad)"
-							+ "VALUES (?,?,?)",
-					Statement.RETURN_GENERATED_KEYS);) {
-				System.out.println("TRY-CATCH");
-
-				ejecutaRegistro(producto, statement);
-				
-				con.commit();
-				System.out.println("COMMIT");
-			} catch (Exception e) {
-				con.rollback();
-				System.out.println("ROLLBACK");
-			}
-		}
+		persistenciaProducto.guardarProducto(producto);
 	}
 
-	private static void ejecutaRegistro(Producto producto, PreparedStatement statement) throws SQLException {
-		statement.setString(1, producto.getNombre());
-		statement.setString(2, producto.getDescripcion());
-		statement.setInt(3, producto.getCantidad());
 
-		statement.execute();
-
-		try(ResultSet resultSet = statement.getGeneratedKeys();){
-			while (resultSet.next()){
-				producto.setId(resultSet.getInt(1));
-				resultSet.getInt(1);
-			}
-		}
-
-	}
 
 }
